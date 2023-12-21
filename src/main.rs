e54b4bba271f684f256fd5d2f336dc055ab9173d
Christian Lehmann <clehmann.geo@gmail.com>
2023-12-21 13:03:57 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use egui_wgpu::renderer::{RenderPass, ScreenDescriptor};
+use egui_wgpu::{renderer::ScreenDescriptor, Renderer};
+use egui_winit::State;
 use std::borrow::Cow;
+use std::iter;
 use wgpu;
+use wgpu::InstanceDescriptor;
 use winit::{
     event::{Event, MouseButton, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
 
-const INITIAL_WIDTH: u32 = 1422;
-const INITIAL_HEIGHT: u32 = 700;
+const INITIAL_WIDTH: u32 = 1920;
+const INITIAL_HEIGHT: u32 = 1080;
 
 const WGSL_SHADERS: &str = "
 struct VertexInput {
@@ -35,63 +38,79 @@ fn fragment_main(in: VertexOutput) -> @location(0) vec4<f32> {
 ";
 
 async fn run(event_loop: EventLoop<()>, window: Window) {
-
     let mut window_size = window.inner_size();
+    window_size.width = window_size.width.max(1);
+    window_size.height = window_size.height.max(1);
 
-    let instance = wgpu::Instance::new(wgpu::Backends::PRIMARY);
-
-    let surface = unsafe { instance.create_surface(&window) };
+    //let instance_descriptor = InstanceDescriptor::default();
+    //let instance = wgpu::Instance::new(instance_descriptor);
+    let instance = wgpu::Instance::default();
+    let surface = unsafe {
+        instance
+            .create_surface(&window)
+            .expect("Error - could not get a surface")
+    };
 
     let adapter = instance
         .request_adapter(&wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::HighPerformance,
+            power_preference: wgpu::PowerPreference::default(),
             compatible_surface: Some(&surface),
             force_fallback_adapter: false,
         })
         .await
         .expect("Failed to find a WebGPU adapter");
 
-    let surface_format = surface.get_supported_formats(&adapter)[0];
-
     let (device, queue) = adapter
         .request_device(
             &wgpu::DeviceDescriptor {
                 label: None,
                 features: wgpu::Features::empty(),
-                limits: wgpu::Limits::default(),
+                limits: wgpu::Limits::downlevel_webgl2_defaults()
+                    .using_resolution(adapter.limits()),
             },
             None,
         )
         .await
         .expect("Failed to create device");
 
+    let capabilities = surface.get_capabilities(&adapter);
+    //let surface_format = *capabilities.formats.iter().find(|f| f.is_srgb()).expect("Could not find srgb surface");
+
     // Chrome Canary complains about unknown swapchain format
     #[cfg(target_arch = "wasm32")]
-    let swap_chain_format = wgpu::TextureFormat::Bgra8Unorm;
+    let surface_format = wgpu::TextureFormat::Bgra8Unorm;
     #[cfg(not(target_arch = "wasm32"))]
-    let swap_chain_format = wgpu::TextureFormat::Bgra8UnormSrgb;
+    let surface_format = wgpu::TextureFormat::Bgra8UnormSrgb;
 
-    surface.configure(
-        &device,
-        &wgpu::SurfaceConfiguration {
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-            format: swap_chain_format,
-            width: window_size.width,
-            height: window_size.height,
-            present_mode: wgpu::PresentMode::Fifo,
-        },
-    );
+    let mut surface_config = wgpu::SurfaceConfiguration {
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format: surface_format,
+        width: window_size.width,
+        height: window_size.height,
+        present_mode: wgpu::PresentMode::Fifo,
+        alpha_mode: capabilities.alpha_modes[0],
+        view_formats: vec![],
+    };
+    surface.configure(&device, &surface_config);
 
-    let mut state = egui_winit::State::new(&event_loop);
     let context = egui::Context::default();
+    context.set_style(egui::Style::default());
     context.set_pixels_per_point(window.scale_factor() as f32);
-    let mut egui_rpass = RenderPass::new(&device, surface_format, 1);
 
-    let vertex_module = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+    let mut state = State::new(context.viewport_id(), &window, None, None);
+    //state.set_pixels_per_point(window.scale_factor() as f32);
+
+    //let mut state = egui_winit::State::new(&event_loop);
+    //let context = egui::Context::default();
+    //context.set_pixels_per_point(window.scale_factor() as f32);
+
+    let mut egui_rpass = Renderer::new(&device, surface_format, None, 1);
+
+    let vertex_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
         label: None,
         source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(WGSL_SHADERS)),
     });
-    let fragment_module = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+    let fragment_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
         label: None,
         source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(WGSL_SHADERS)),
     });
@@ -186,19 +205,27 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         fragment: Some(wgpu::FragmentState {
             module: &fragment_module,
             entry_point: "fragment_main",
-            targets: &[wgpu::ColorTargetState {
-                format: swap_chain_format,
+            targets: &[Some(wgpu::ColorTargetState {
+                format: surface_format,
                 blend: Some(wgpu::BlendState::ALPHA_BLENDING),
                 write_mask: wgpu::ColorWrites::ALL,
-            }],
+            })],
         }),
         multiview: None,
     });
 
-    let clear_color = wgpu::Color::TRANSPARENT;
-
     event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Poll;
+        //*control_flow = ControlFlow::Poll;
+
+        if let Event::WindowEvent { event, .. } = &event {
+            let response = state.on_window_event(&context, event);
+            if response.repaint {
+                window.request_redraw();
+            }
+            if response.consumed {
+                return;
+            }
+        }
 
         match event {
             Event::WindowEvent { event, .. } => match event {
@@ -217,44 +244,66 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                         window_size.height = size.height;
                         println!("scale factor: {:?}", window.scale_factor());
                         println!("w/h: {:?} {:?}", window_size.width, window_size.height);
-                        println!("used size: {:?} {:?}", context.used_size().x, context.used_size().y);
+                        println!(
+                            "used size: {:?} {:?}",
+                            context.used_size().x,
+                            context.used_size().y
+                        );
                         surface.configure(
                             &device,
                             &wgpu::SurfaceConfiguration {
                                 usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-                                format: swap_chain_format,
+                                format: surface_format,
                                 width: window_size.width,
                                 height: window_size.height,
                                 present_mode: wgpu::PresentMode::Fifo,
+                                alpha_mode: capabilities.alpha_modes[0],
+                                view_formats: vec![],
                             },
                         );
                     }
                 }
                 WindowEvent::DroppedFile(file) => {
-                    //println!("File dropped: {:?}", file.as_path().display().to_string());
+                    println!("File dropped: {:?}", file.as_path().display().to_string());
                     // do model loading here
                 }
-                WindowEvent::CursorMoved { position, .. } =>
-                {
-                   println!("pos_x: {:?} pos_y: {:?}", position.x, position.y);
-                   /*if context.is_pointer_over_area()*/ { state.on_event(&context, &event); }
+                WindowEvent::CursorMoved { position, .. } => {
+                    println!("pos_x: {:?} pos_y: {:?}", position.x, position.y);
+                    /*if context.is_pointer_over_area()*/
+                    {
+                        state.on_window_event(&context, &event);
+                    }
                 }
                 _ => {
                     // forward events to egui
-                    state.on_event(&context, &event);
+                    state.on_window_event(&context, &event);
                 }
             },
             Event::MainEventsCleared => {
-                let output_frame = surface
-                    .get_current_texture()
-                    .expect("Failed to get surface output texture");
-                let render_target_view = output_frame
+                window.request_redraw();
+            }
+            Event::RedrawRequested(..) => {
+                let output_frame = match surface.get_current_texture() {
+                    Ok(frame) => frame,
+                    Err(wgpu::SurfaceError::Outdated) => {
+                        // This error occurs when the app is minimized on Windows.
+                        // Silently return here to prevent spamming the console with:
+                        // "The underlying surface has changed, and therefore the swap chain must be updated"
+                        return;
+                    }
+                    Err(e) => {
+                        eprintln!("Dropped frame with error: {}", e);
+                        return;
+                    }
+                };
+                let output_view = output_frame
                     .texture
                     .create_view(&wgpu::TextureViewDescriptor::default());
 
                 let input = state.take_egui_input(&window);
 
                 context.begin_frame(input);
+
                 egui::Window::new("K4 Kahlberg").show(&context, |ui| {
                     ui.heading("Objects");
                     ui.label("Currently there are no objects here.");
@@ -267,11 +316,12 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                         //egui::widgets::global_dark_light_mode_switch(ui);
                         context.set_visuals(egui::Visuals::light());
                     }
-                    
                 });
                 let output = context.end_frame();
 
-                let paint_jobs = context.tessellate(output.shapes);
+                let paint_jobs = context.tessellate(output.shapes, context.pixels_per_point());
+
+                state.handle_platform_output(&window, &context, output.platform_output);
 
                 let mut encoder =
                     device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
@@ -282,26 +332,29 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                     pixels_per_point: window.scale_factor() as f32,
                 };
 
-                for (id, image_delta) in &output.textures_delta.set {
-                    egui_rpass.update_texture(&device, &queue, *id, image_delta);
+                let tdelta: egui::TexturesDelta = output.textures_delta;
+                for (tid, deltas) in tdelta.set {
+                    egui_rpass.update_texture(&device, &queue, tid, &deltas);
                 }
-                for id in &output.textures_delta.free {
-                    egui_rpass.free_texture(id);
-                }
-                egui_rpass.update_buffers(&device, &queue, &paint_jobs, &screen_descriptor);
+                egui_rpass.update_buffers(
+                    &device,
+                    &queue,
+                    &mut encoder,
+                    &paint_jobs,
+                    &screen_descriptor,
+                );
 
                 {
                     let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                         label: None,
-                        color_attachments: &[wgpu::RenderPassColorAttachment {
-                            view: &render_target_view,
+                        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                            view: &output_view,
                             resolve_target: None,
-                            ops: wgpu::Operations {
-                                load: wgpu::LoadOp::Clear(clear_color),
-                                store: true,
-                            },
-                        }],
+                            ops: Default::default(),
+                        })],
                         depth_stencil_attachment: None,
+                        timestamp_writes: None,
+                        occlusion_query_set: None,
                     });
 
                     render_pass.set_pipeline(&render_pipeline);
@@ -320,14 +373,14 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                         render_pass.draw(0..3, 0..1);
                     }
 
-                    egui_rpass.execute_with_renderpass(
-                        &mut render_pass,
-                        &paint_jobs,
-                        &screen_descriptor,
-                    );
+                    egui_rpass.render(&mut render_pass, &paint_jobs, &screen_descriptor);
                 }
-                queue.submit(Some(encoder.finish()));
+                queue.submit(iter::once(encoder.finish()));
                 output_frame.present();
+
+                for tid in tdelta.free {
+                    egui_rpass.free_texture(&tid);
+                }
             }
             _ => (),
         }
@@ -335,34 +388,44 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
 }
 
 fn main() {
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_transparent(true)
+    let event_loop = winit::event_loop::EventLoopBuilder::<()>::with_user_event().build();
+    #[allow(unused_mut)]
+    let mut builder = winit::window::WindowBuilder::new()
         .with_title("K4 Kahlberg")
         .with_inner_size(winit::dpi::PhysicalSize {
             width: INITIAL_WIDTH,
             height: INITIAL_HEIGHT,
-        })
-        .build(&event_loop)
-        .unwrap();
+        });
+
     #[cfg(not(target_arch = "wasm32"))]
     {
-        futures::executor::block_on(run(event_loop, window));
+        env_logger::init();
+        pollster::block_on(run(event_loop, window));
     }
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        use wasm_bindgen::JsCast;
+        use winit::platform::web::WindowBuilderExtWebSys;
+        let canvas = web_sys::window()
+            .expect("error window")
+            .document()
+            .expect("error document")
+            .get_element_by_id("canvas")
+            .expect("error anchor")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("error HtmlCanvasElement");
+        builder = builder.with_canvas(Some(canvas));
+    }
+
+    let window = builder
+        .build(&event_loop)
+        .expect("Could not create Event Loop!");
+
     #[cfg(target_arch = "wasm32")]
     {
         std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-        console_log::init().expect("Failed to initialize logger");
-        use winit::platform::web::WindowExtWebSys;
-        // On wasm, append the canvas to the document body
-        web_sys::window()
-            .and_then(|win| win.document())
-            .and_then(|doc| doc.body())
-            .and_then(|body| {
-                body.append_child(&web_sys::Element::from(window.canvas()))
-                    .ok()
-            })
-            .expect("Failed to append canvas to body");
+        console_log::init_with_level(log::Level::Warn).expect("could not initialize logger");
         wasm_bindgen_futures::spawn_local(run(event_loop, window));
     }
 }
